cbdca7732b5a793cfd30bfe5555073c0262e3cdf
ryanzomorrodi <ryanzomorrodi@gmail.com>
2024-09-15 02:56:54 -0500

    untidy ha_stratifications

--- R/ha_stratifications.R
+++ R/ha_stratifications.R
@@ -1,15 +1,12 @@
 ha_stratifications <- function(progress = TRUE) {
   body <- ha_api_stratifications_req() |>
     ha_req_perform_iterative(progress) |>
-    ha_resp_body_iterative()
+    lapply(\(x) httr2::resp_body_json(x, simplifyVector = TRUE)) |>
+    lapply(\(x) x[["results"]])
 
-  tibble::tibble(body) |>
-    tidyr::unnest_wider(body) |>
-    dplyr::select(
-      c(
-        "population_key" = "key",
-        "population_name" = "name",
-        "population_grouping" = "grouping"
-      )
-    )
+  output <- do.call(rbind, body)
+  output <- output[c("key", "name", "grouping")]
+  colnames(output) <- c("population_key", "population_name", "population_grouping")
+
+  output
 }
